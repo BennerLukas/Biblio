d75bf58d696b1dadeef3183b34e85e1f482053e1
BennerLukas <lukas_benner@outlook.de>
2021-02-01 16:33:37 +0100

    add transaciton [not working]

---
+++ src/database/transaction.sql
@@ -0,0 +1,54 @@
+DECLARE 
+        author_first_name   VARCHAR(128)    := 'Klaus'
+        author_last_name    VARCHAR(128)    := 'Kleber';
+        author_address      INT             :=  NULL;       
+        publisher_name      VARCHAR(128)    := 'K-Verlag';
+        publisher_address   INT             :=  NULL;       
+        book_title          VARCHAR(4096)   := 'Die Suche';
+        book_edition        INT             :=  1;
+        book_language       CHAR(3)         := 'DE';
+        book_genre          CHAR(20)        := 'Fantasy';
+        book_isbn           VARCHAR(13)     := '1234567891234';
+
+-- without addresses
+
+BEGIN
+    IF NOT EXISTS   (   SELECT n_author_id
+                    FROM author
+                    WHERE   author_last_name = s_last_name
+                    AND     author_first_name = s_first_name)
+                )
+    BEGIN
+        INSERT INTO AUTHOR(s_first_name, s_last_name, n_address_id)
+        VALUES (author_first_name, author_last_name, author_address)
+    END
+
+    IF NOT EXITS    (   SELECT n_publisher_id
+                        FROM PUBLISHER
+                        WHERE publisher_name = s_pub_name)
+    BEGIN
+        INSERT INTO PUBLISHER(s_pub_name, n_address_id)
+        VALUES (publisher_name, publisher_address)
+    END
+
+    IF NOT EXISTS   (   SELECT n_book_id
+                        FROM books
+                        WHERE   book_isbn = s_isbn
+                        OR      (book_title = s_title AND book_edition = n_book_edition AND book_language = s_book_language)
+                        )
+    BEGIN
+        INSERT INTO BOOKS(s_isbn, s_title, n_book_edition, s_genre, dt_publishing_date, s_book_language, n_recommended_age, b_is_availalbe, n_publisher_id, n_location_id)
+        VALUES  (   book_isbn, 
+                    book_title,  
+                    book_edition, 
+                    book_genre, 
+                    NULL, 
+                    book_language, 
+                    NULL, 
+                    true, 
+                    (SELECT n_author_id FROM author WHERE   author_last_name = s_last_name AND author_first_name = s_first_name),
+                    (SELECT n_publisher_id FROM PUBLISHER WHERE publisher_name = s_pub_name)    --2nd time executing same query --> Performance?!
+                    )
+    END
+
+END;
--- src/database/view.sql
+++ src/database/view.sql
@@ -1,8 +1,34 @@
 DROP VIEW IF EXISTS overview;
 CREATE VIEW overview AS
-SELECT books.s_title Title, books.s_genre Genre, STRING_AGG (s_last_name, ', ') Author, s_pub_name Publisher
+SELECT  books.s_title Title, 
+        books.s_genre Genre,
+        books.s_isbn Isbn, 
+        STRING_AGG (s_last_name, ', ') Author, 
+        s_pub_name Publisher
 FROM books
 LEFT JOIN wrote ON (books.n_book_id = wrote.n_book_id)
 LEFT JOIN author ON (wrote.n_author_id = author.n_author_id)
 LEFT JOIN publisher ON (books.n_publisher_id = publisher.n_publisher_id)
 GROUP BY books.n_book_id, s_pub_name;
+
+
+
+DROP VIEW IF EXISTS book_extended;
+CREATE VIEW book_extended AS
+SELECT      books.n_book_id, 
+            books.s_isbn, 
+            books.s_title , 
+            books.n_book_edition, 
+            books.s_genre, 
+            books.dt_publishing_date, 
+            books.s_book_language, 
+            books.n_recommended_age, 
+            books.b_is_availalbe, 
+            author.s_first_name, 
+            author.s_last_name, 
+            publisher.s_pub_name
+FROM books
+LEFT JOIN wrote ON (books.n_book_id = wrote.n_book_id)
+LEFT JOIN author ON (wrote.n_author_id = author.n_author_id)
+LEFT JOIN publisher ON (books.n_publisher_id = publisher.n_publisher_id)
+GROUP BY books.n_book_id, wrote.n_wrote_id, author.n_author_id, publisher.n_publisher_id;
