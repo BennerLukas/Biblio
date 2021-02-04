17abeb75afee5747ac3dadd404308c58bbdf9256
BennerLukas <lukas_benner@outlook.de>
2021-02-04 13:36:26 +0100

    working w/o trigger

--- src/database/procedure.sql
+++ src/database/procedure.sql
@@ -1,4 +1,3 @@
--- if loan happened --> change is available
 create or replace procedure new_loan(
     loan_user int,
     book int,
@@ -10,31 +9,15 @@ as '
 
         INSERT INTO LOAN (ts_now, n_user_id)
         VALUES(now(), loan_user);
-       
-    
+
         INSERT INTO BORROW_ITEM (n_duration, n_book_id, n_loan_id)
         VALUES (duration, book,(SELECT n_loan_id FROM LOAN ORDER BY n_loan_id DESC LIMIT 1));
-    COMMIT; 
+        
+        CALL last_not_available();
+		END;
+	'
 
-	end;'
-
-
-
-
--- create or replace procedure last_not_available(
--- )
--- language plpgsql
--- as '
---     BEGIN
---         UPDATE books
---         SET b_is_availalbe = false
---         WHERE n_book_id = (SELECT n_book_id FROM borrow_item WHERE n_loan_id = (SELECT n_loan_id FROM LOAN ORDER BY n_loan_id DESC LIMIT 1));
---     COMMIT; 
--- 	end;'
-
-
-create or replace function last_not_available()
-RETURNS TRIGGER
+create or replace procedure last_not_available()
 language plpgsql
 as '
    BEGIN
