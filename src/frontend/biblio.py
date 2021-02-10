4ee9714daf8019ebd458b758c036a4bc4d47dbbb
BennerLukas <lukas_benner@outlook.de>
2021-02-10 11:08:36 +0100

    basic structure

--- src/frontend/biblio.py
+++ src/frontend/biblio.py
@@ -4,5 +4,21 @@ import sqlalchemy
 class Biblio:
 
     def __init__(self):
-        self.user = None
+        self.s_user = None
+        self.b_is_connected = False
 
+    def connect(self):
+        pass
+
+    def list_read_books(self):
+        
+        books = []
+        return books
+    
+    def make_loan(self, book_ids, duration):
+        
+        return True
+
+    def return_book(self, book_id):
+        
+        return True
