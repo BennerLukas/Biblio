d830749f682693d3f143a58d36f1a7f2adb0cba0
BennerLukas <lukas_benner@outlook.de>
2021-02-12 10:26:44 +0100

    [NOT WORKING] example scripting

--- src/frontend/biblio.py
+++ src/frontend/biblio.py
@@ -22,14 +22,26 @@ class Biblio:
             self.connection = self.engine.raw_connection()
             self.cursor = self.connection.cursor()
             self.b_connected = True
+            print("Done")
             logging.info("Connected to DB")
         except:
             logging.error("Not connected to DB")
             return False
         return True
 
+    def test(self):
+        
+        pass 
+
+
     def init_db(self):
-        pass
+        s_sql_statement = open(r"src\database\init.sql", "r").read()
+        print(s_sql_statement)
+
+        result = self.engine.execute(s_sql_statement)
+
+        logging.info("Database initialised")
+        return True
 
     def list_read_books(self):
         
@@ -46,3 +58,4 @@ class Biblio:
 
 if __name__ == "__main__":
     my_class = Biblio()
+    my_class.init_db()
