f796aedbd89023d82c4598269021106e32368ef1
BennerLukas <lukas_benner@outlook.de>
2021-03-06 12:26:59 +0100

    add heading title

--- src/frontend/app/views.py
+++ src/frontend/app/views.py
@@ -8,41 +8,29 @@ from api.biblio import Biblio
 bib = Biblio()
 
 
-@app.route('/')                                         # Home
+@app.route('/')  # Home
 def index():
     return render_template("/index.html")
 
 
-@app.route('/books')                                    # Books
+@app.route('/books')  # Books
 def books():
     return render_template("books.html")
 
 
-@app.route('/profile')                                  # Profile
+@app.route('/profile')  # Profile
 def profile():
     return render_template("profile.html")
 
 
-@app.route('/return_book', methods=['POST', 'GET'])     # Return Book
+@app.route('/return_book', methods=['POST', 'GET'])  # Return Book
 def return_book():
-    if request.method == 'POST':
-        book_id = request.form["book_id"]
-        result = bib.return_book(book_id)
-        if result is True:
-            result_text = f"Book {book_id} successfully returned."
-            return render_template("success.html", return_book_result=result_text)
-        else:
-            result_text = f"Book {book_id} couldn't be returned."
-            return render_template("fail.html", return_book_result=result_text)
+    return render_template("return_book.html")
 
 
-@app.route('/list_read_books', methods=['POST', 'GET'])     # Reading History
+@app.route('/list_read_books', methods=['POST', 'GET'])  # Reading History
 def list_read_books():
-    if request.method == 'POST':
-        print(request.form)
-        result = bib.list_read_books()
-        print(result.head())
-        return render_template("includes/table.html", tables=[result.to_html(classes="data")], titles=result.columns.values)
+    return render_template("list_read_books.html")
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -57,12 +45,12 @@ def search():
 
 @app.route('/active_loans', methods=['POST', 'GET'])
 def active_loans():
-    return render_template("table.html")
+    return render_template("loans_active.html")
 
 
 @app.route('/loan_history', methods=['POST', 'GET'])
 def loan_history():
-    return render_template("table.html")
+    return render_template("loans_history.html")
 
 
 @app.route('/about')
@@ -78,6 +66,6 @@ def settings():
 ###########################################################################
 
 
-@app.route('/dev', methods=['POST', 'GET'])                       # Testing
+@app.route('/dev', methods=['POST', 'GET'])  # Testing
 def dev():
-    return render_template("dev.html")
+    return render_template("_dev.html")
