6be3739c3da12f889ada43e6b78e2c4a6cd2d3a0
Phillip Lange <phillip@lange-privat.net>
2021-03-20 18:37:19 +0100

    added existing queries to a homepage

--- src/frontend/api/selections.py
+++ src/frontend/api/selections.py
@@ -4,16 +4,25 @@ class Selections:
         pass
 
     @staticmethod
-    def sql_exceeded_loans():
-        s_sql = """
-        SELECT 
-            bi.n_borrow_item_id AS Borrowed_item, 
-            bi.n_loan_id AS Loan_id, 
-            bi.n_duration + DATE(l.ts_now) AS "Due Date"
-        FROM borrow_item AS bi
-            LEFT JOIN loan AS l ON bi.n_loan_id = l.n_loan_id
-        WHERE CURRENT_DATE > bi.n_duration + DATE(l.ts_now) AND bi.b_active = 'TRUE';
-        """
+    def sql_exceeded_loans(info=False):
+        if not info:
+            s_sql = """
+            SELECT 
+                bi.n_borrow_item_id AS Borrowed_item, 
+                bi.n_loan_id AS Loan_id, 
+                bi.n_duration + DATE(l.ts_now) AS "Due Date"
+            FROM borrow_item AS bi
+                LEFT JOIN loan AS l ON bi.n_loan_id = l.n_loan_id
+            WHERE CURRENT_DATE > bi.n_duration + DATE(l.ts_now) AND bi.b_active = 'TRUE';
+            """
+        else:
+            s_sql = """
+            SELECT COUNT(DISTINCT (n_borrow_item_id)) AS Amount
+            FROM borrow_item AS bi
+                LEFT JOIN loan AS l ON bi.n_loan_id = l.n_loan_id
+            WHERE CURRENT_DATE > bi.n_duration + DATE(l.ts_now)
+                AND bi.b_active = 'TRUE'
+            """
         return s_sql
 
     @staticmethod
