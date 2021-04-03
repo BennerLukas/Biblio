2fd8aa196b2ab188f196231f5679192ad58361b6
Sabokou <52411026+Sabokou@users.noreply.github.com>
2021-04-03 17:42:25 +0200

    implemented waiting function to fix not being able to connect to database during spin-up of postgres container

--- src/code/init.py
+++ src/code/init.py
@@ -1,6 +1,30 @@
 # run.py
+import psycopg2
+import time
+import logging
 
 from app.biblio import Biblio
 
+
+def try_init(retries=10):
+    calc_retries = retries
+    connection = False
+    while retries > 0 and connection is False:
+        try:
+            con = psycopg2.connect(database="postgres", user="postgres", port=5432,
+                                   password="1234", host="database")
+        except psycopg2.OperationalError:
+            retries -= 1
+            logging.error(f"Could not connect to database during initialization try {-1 * retries + calc_retries}")
+            time.sleep(5)
+        else:
+            retries -= 1
+            connection = True
+            return retries
+
+
 if __name__ == '__main__':
+    count = try_init()
+    logging.info(f"Remaining retries: {count}")
+    logging.error("Connection possible -- trying to initialize database")
     Biblio().init_db()
